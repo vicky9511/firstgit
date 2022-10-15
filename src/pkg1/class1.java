9ac9ce63234402b3983a230fcbb7bd455af3bb3d
vikrant <vikrantpawade4878@gmail.com>
2022-10-15 12:44:37 +0530

    add my project

---
+++ src/pkg1/class1.java
@@ -0,0 +1,21 @@
+package pkg1;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		int marks = 85;
+		int age = 20;
+		
+		
+		if(marks >= 90 && age >= 18)
+		{
+			System.out.println("he will get bike");
+		}
+		else
+		{
+			System.out.println("he will not get bike");
+		}
+
+	}
+
+}
